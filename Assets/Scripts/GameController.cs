f0995ed2530767905508af593012bde533fa3ade
Jack Simunic <jacksimunic2017@gmail.com>
2021-12-25 01:01:20 -0800

    files

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,121 @@
+﻿using UnityEngine;
+using Assets.Scripts;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public enum Player { NoMark, Naught, Cross, Draw }
+public class GameController : MonoBehaviour
+{
+    // gaming golem
+    public GameObject markerPrefab;
+    public static event Action<Player> gameWin;
+    public Player turn;
+    public Player winner;
+    public Player[,] markers;
+    [SerializeField] public Vector3[,] markerSpots;
+    public List<GameObject> markerObjects;
+    public static bool playerHasWon = false;
+    void Start()
+	{
+		Physics.queriesHitTriggers = true;
+		markerObjects = new List<GameObject>();
+		ResetGame();
+		CanvasScript.resetGame += () =>
+		{
+            ResetGame();
+		};
+		void SetMarkerArray()
+		{
+			markers = new Player[3, 3];
+			for (int x = 0; x < 3; x++)
+			{
+				for (int y = 0; y < 3; y++)
+				{
+					markers[x, y] = Player.NoMark;
+				}
+			}
+		}
+
+		void ResetGame()
+		{
+			turn = Player.Cross;
+			SetMarkerArray();
+			SetMarkerSpots();
+			foreach (var item in markerObjects)
+			{
+				Destroy(item);
+			}
+			markerObjects.Clear();
+			playerHasWon = false;
+		}
+
+	    void SetMarkerSpots()
+        {
+            markerSpots = new Vector3[3, 3];
+            markerSpots[0, 0] = new Vector3(-2.8f, -2.8f, -1f);
+            markerSpots[1, 0] = new Vector3(0f, -2.8f, -1f);
+            markerSpots[2, 0] = new Vector3(2.8f, -2.8f, -1f);
+            markerSpots[0, 1] = new Vector3(-2.8f, 0f, -1f);
+            markerSpots[1, 1] = new Vector3(0f, 0f, -1f);
+            markerSpots[2, 1] = new Vector3(2.8f, 0f, -1f);
+            markerSpots[0, 2] = new Vector3(-2.8f, 2.8f, -1f);
+            markerSpots[1, 2] = new Vector3(0f, 2.8f, -1f);
+            markerSpots[2, 2] = new Vector3(2.8f, 2.8f, -1f);
+        }
+	}
+    void OnMouseDown()
+    {
+        if (PauseMenu.isGamePaused)
+            return;
+        if (playerHasWon)
+            return;
+        Vector2 p = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        TryPlaceMarker(p);
+        winner = FindWinner();
+        if (winner != Player.NoMark)
+        {
+            playerHasWon = true;
+            //Calling gameWin
+            gameWin(winner);
+            Debug.Log($"{winner} won the game!");
+        }
+
+        void TryPlaceMarker(Vector2 pos)
+        {
+            (int x, int y) spot = WinCalculator.GetNearestSpot(pos, markerSpots);
+            if (markers[spot.x, spot.y] != Player.NoMark)
+		    {
+                Debug.Log("That spot is taken.");
+                return;
+		    }
+            GameObject marker = Instantiate(markerPrefab, markerSpots[spot.x, spot.y], new Quaternion(0, 0, 0, 0));
+            markerObjects.Add(marker);
+            markers[spot.x, spot.y] = turn;
+            marker.GetComponent<MarkerScript>().team = turn;
+            if (turn == Player.Cross)
+            {
+                turn = Player.Naught;
+            }
+            else
+            {
+                turn = Player.Cross;
+            }
+        }
+
+        Player FindWinner()
+        {
+            if (FindIfWon(Player.Cross)) return Player.Cross;
+            if (FindIfWon(Player.Naught)) return Player.Naught;
+            if (markerObjects.Count == 9) return Player.Draw;
+            return Player.NoMark;
+        }
+
+        bool FindIfWon(Player player)
+        {
+            return (WinCalculator.FindRowWin(player, markers) || WinCalculator.FindColWin(player, markers) || WinCalculator.FindDiagWin(player, markers));
+        }
+    }
+
+}
---
+++ Assets/Scripts/WinCalculator.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    static class WinCalculator
+    {
+        public static Player OtherMark(this Player p)
+		{
+            if (p == Player.NoMark)
+                throw new System.ArgumentException("Tried to find the other player to a NoMark.");
+			if (p == Player.Naught)
+                return Player.Cross;
+            return Player.Naught;
+		}
+        public static (int x, int y) GetNearestSpot(Vector2 position, Vector3[,] markerSpots)
+        {
+            float lowestDist = 100f;
+            float currentDist = 0f;
+            (int, int) closestSpot = (2, 0);
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    currentDist = Vector2.Distance(position, markerSpots[x, y]);
+                    if (currentDist < lowestDist)
+                    {
+                        lowestDist = currentDist;
+                        closestSpot = (x, y);
+                    }
+                }
+            }
+            return closestSpot;
+        }
+        public static bool FindDiagWin(Player player, Player[,] markers)
+        {
+            bool[] diagWin = { true, true };
+            for (int x = 0; x < 3; x++)
+            {
+                if (markers[x, x] != player) diagWin[0] = false;
+            }
+            int y = 0;
+            for (int x = 2; x >= 0; x--)
+            {
+                if (markers[x, y] != player) diagWin[1] = false;
+                y++;
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                if (diagWin[i]) return true;
+            }
+
+            return false;
+        }
+        public static bool FindColWin(Player player, Player[,] markers)
+        {
+            bool[] colWin = { true, true, true };
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    if (markers[x, y] != player) colWin[x] = false;
+                }
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                if (colWin[i]) return true;
+            }
+
+            return false;
+        }
+        public static bool FindRowWin(Player player, Player[,] markers)
+        {
+            bool[] rowWin = { true, true, true };
+            for (int y = 0; y < 3; y++)
+            {
+                for (int x = 0; x < 3; x++)
+                {
+                    if (markers[x, y] != player) rowWin[y] = false;
+                }
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                if (rowWin[i]) return true;
+            }
+            return false;
+        }
+    }
+}
